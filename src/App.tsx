7f562e8f9ea627ce2b5b24699ece03bb29b744f4
uumair327 <mohdumair.a@somaiya.edu>
2024-09-27 21:34:19 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
